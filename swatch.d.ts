a16f18ab970f6eb0896a73a1ac0d7f50456de17d
Robert Soriano <sorianorobertc@gmail.com>
2022-05-06 14:22:33 -0700

    add types

---
+++ swatch.d.ts
@@ -0,0 +1,18 @@
+import * as Vue from "vue";
+
+declare const _default: Vue.DefineComponent<
+  {},
+  {},
+  {},
+  {},
+  {},
+  Vue.ComponentOptionsMixin,
+  Vue.ComponentOptionsMixin,
+  Vue.EmitsOptions,
+  string,
+  Vue.VNodeProps & Vue.AllowedComponentProps & Vue.ComponentCustomProps,
+  Readonly<Vue.ExtractPropTypes<{}>>,
+  {}
+>;
+
+export default _default;
