79230ce8b7fb7ac3a89689acfec1335275de86dc
wobsoriano <sorianorobertc@gmail.com>
2022-11-14 13:41:50 -0800

    add nuxt component types

--- nuxt/module.mjs
+++ nuxt/module.mjs
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url'
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addTemplate } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
@@ -10,6 +10,23 @@ export default defineNuxtModule({
     const { resolve } = createResolver(import.meta.url)
     const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
     nuxt.options.build.transpile.push(runtimeDir)
+
+    addTemplate({
+      filename: 'types/vue3-swatches.d.ts',
+      getContents: () => `
+        declare module '@vue/runtime-core' {
+          export interface GlobalComponents {
+            'VSwatches': typeof import('vue3-swatches')['VSwatches']
+          }
+        }
+
+        export const VSwatches: typeof import('vue3-swatches')['VSwatches']
+      `
+    })
+
+    nuxt.hook('prepare:types', (options) => {
+      options.references.push({ path: resolve(nuxt.options.buildDir, 'types/vue3-swatches.d.ts') })
+    })
 
     addPlugin(resolve(runtimeDir, 'plugin.client.mjs'))
   }
